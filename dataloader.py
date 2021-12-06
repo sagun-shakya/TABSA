fc9daa159d34b744c0e6dc4c749fadfbcebd91e4
sagun-shakya <sagun.shakya@naamii.org.np>
2021-12-06 10:13:52 +0545

    Added to-to list and changed the name of the main Class.

--- dataloader.py
+++ dataloader.py
@@ -4,8 +4,11 @@ Created on Wed Dec  1 09:33:47 2021
 
 @author: Sagun Shakya
 """
-
-class DataLoader:
+'''
+TO-DO:
+    - Add Embedding matrix. see https://github.com/oya163/nepali-sentiment-analysis/blob/master/utility/preprocess/word_vectors.py
+'''
+class DataLoaderConstant:
     def __init__(self, vocab_size, tagset_size, weights):
         self.vocab_size = vocab_size
         self.tagset_size = tagset_size
