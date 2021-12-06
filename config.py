50e057cd72a552d348ea219d3715d18aa92c5185
sagun-shakya <sagun.shakya@naamii.org.np>
2021-12-06 10:12:10 +0545

    Added batch_size to configuration.

--- config.py
+++ config.py
@@ -5,11 +5,12 @@ Created on Wed Dec  1 09:12:53 2021
 @author: Sagun Shakya
 """
 
-class Config:
+class Configuration:
     '''Configuration for parameters for the LSTM model.'''
     
-    def __init__(self, bidirection, num_layers, hidden_dim, embedding_dim, dropout_prob, pretrained, train_type):
+    def __init__(self, bidirection, batch_size, num_layers, hidden_dim, embedding_dim, dropout_prob, pretrained, train_type):
         self.bidirection = bidirection
+        self.batch_size = batch_size
         self.num_layers = num_layers
         self.hidden_dim = hidden_dim
         self.embedding_dim = embedding_dim
