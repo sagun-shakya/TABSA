0696a4e4f2a81616cb94ee6123868544b59c35d6
sagun-shakya <sagun.shakya@naamii.org.np>
2021-12-07 10:00:37 +0545

    Accomodation for aspect, target and combined extraction tasks.

--- load_data.py
+++ load_data.py
@@ -10,7 +10,7 @@ import numpy as np
 import pandas as pd
 
 
-def get_file_in_df(filename):
+def get_file_in_df(filename, mode = 'aspect'):
     '''
     Gets the file in a DataFrame.
     
@@ -47,5 +47,29 @@ def get_file_in_df(filename):
     
     df = pd.concat([sentence_no, df], axis = 1).replace('_', np.nan)
     df.dropna(inplace = True)
-    return df
     
+    aspect_categories = ['B-GENERAL', 'I-GENERAL', 
+                         'B-PROFANITY', 'I-PROFANITY', 
+                         'B-VIOLENCE', 'I-VIOLENCE', 
+                         'B-FEEDBACK', 'I-FEEDBACK']
+    
+    target_categories = ['B-PER', 'I-PER',  
+                         'B-ORG', 'I-ORG', 
+                         'B-LOC', 'I-LOC', 
+                         'B-MISC', 'I-MISC']
+    
+    # Function to remove aspect/target categories from data['tag'].
+    remove_function = lambda series, category: series.replace(category, ['O']*len(category)) 
+    
+    if mode == 'aspect' or mode == 'ASPECT':
+        # Removing target categories from the set of tags so that we are left with aspect categories only.
+        df['tag'] = remove_function(df['tag'], target_categories) 
+        
+    elif mode == 'target' or mode == 'TARGET':
+        # Removing aspect categories from the set of tags so that we are left with target categories only.
+        df['tag'] = remove_function(df['tag'], aspect_categories)
+        
+    elif mode == 'combined' or mode == 'COMBINED':
+        pass
+    
+    return df        
