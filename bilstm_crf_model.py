71858953541b36742f86a6c411b4c18e5c2278d7
sagun-shakya <sagun.shakya@naamii.org.np>
2021-12-15 22:53:24 +0545

    added lstm_features and viterbi decoder methods.

--- bilstm_crf_model.py
+++ bilstm_crf_model.py
@@ -137,6 +137,28 @@ class BiLSTM_CRF(nn.Module):
     def _get_list_average(self, mylist):
         return sum(mylist) / len(mylist)
     
+    def _lstm_features(self, sentence, seq_len, tag):
+        """Gets the LSTM features for a batch."""
+        
+        # Get word embeddings in a sentence.
+        sent_embedding = self.embedding(sentence)
+        
+        # Packing the output of the embedding layer.
+        packed_input = pack_padded_sequence(sent_embedding, 
+                                            lengths = seq_len.clamp(max = 192), 
+                                            batch_first = True, 
+                                            enforce_sorted = False)
+        
+        # BiLSTM layer.
+        packed_output, (h_t, c_t) = self.bilstm(packed_input)
+
+        # Inverting the packing operation.
+        out, input_sizes = pad_packed_sequence(packed_output, batch_first = True, total_length = self.max_len)
+
+        # Linear layer.
+        linear = self.fc(out)
+        return linear
+    
     def forward(self, sentence, seq_len, tag):
         '''
         Forward pass in training.
@@ -175,4 +197,75 @@ class BiLSTM_CRF(nn.Module):
         nll_loss = Variable(self._get_list_average([self.neg_log_likelihood(sent, tag) for sent, tag in zip(linear, tag)]), requires_grad = True)     
         
         return nll_loss
-
+    
+    def _viterbi_decode(self, feats):
+        """Implements Viterbi algorithm for finding most likely sequence of labels.
+        Used in the forward pass of the network.
+    
+        We take the maximum over the previous states as opposed to the sum. 
+        Input:
+            loglikelihoods: torch tensor.
+        Output:
+            tuple. The first entry is the loglikelihood of this sequence. The second is 
+            the most likely sequence of labels. 
+        """
+        backpointers = []
+    
+        # Initialize the viterbi variables in log space
+        init_vvars = torch.full((1, self.num_tags), -10000.)
+        init_vvars[0][self.tag2idx[self.START_TAG]] = 0
+    
+        # forward_var at step i holds the viterbi variables for step i-1
+        forward_var = init_vvars
+        
+        for feat in feats:
+            bptrs_t = []  # holds the backpointers for this step
+            viterbivars_t = []  # holds the viterbi variables for this step
+    
+            for next_tag in range(self.num_tags):
+                # next_tag_var[i] holds the viterbi variable for tag i at the
+                # previous step, plus the score of transitioning
+                # from tag i to next_tag.
+                # We don't include the emission scores here because the max
+                # does not depend on them (we add them in below)
+                next_tag_var = forward_var + self.transitions[next_tag]
+                best_tag_id = argmax(next_tag_var)
+                bptrs_t.append(best_tag_id)
+                viterbivars_t.append(next_tag_var[0][best_tag_id].view(1))
+           
+            # Now add in the emission scores, and assign forward_var to the set
+            # of viterbi variables we just computed
+            forward_var = (torch.cat(viterbivars_t) + feat).view(1, -1)
+            backpointers.append(bptrs_t)
+    
+        # Transition to STOP_TAG
+        terminal_var = forward_var + self.transitions[self.tag2idx[self.STOP_TAG]]
+        best_tag_id = argmax(terminal_var)
+        path_score = terminal_var[0][best_tag_id]
+    
+        # Follow the back pointers to decode the best path.
+        best_path = [best_tag_id]
+        for bptrs_t in reversed(backpointers):
+            best_tag_id = bptrs_t[best_tag_id]
+            best_path.append(best_tag_id)
+            
+        # Pop off the start tag (we dont want to return that to the caller)
+        start = best_path.pop()
+        assert start == self.tag2idx[self.START_TAG]  # Sanity check
+        best_path.reverse()
+        return path_score, best_path
+    
+    def decode(self, sentence, seq_len, tag):
+        '''Apply Viterbi algorithm to generate best tags.
+        Used for validation only.'''
+        
+        lstm_feature = self._lstm_features(sentence, seq_len, tag)    # Shape (batch_size, max_len, num_tags)
+        best_path_scores = []
+        best_path = []
+        
+        for feature in lstm_feature:
+            path_score_sent, path_sent = self._viterbi_decode(feature)
+            best_path_scores.append(path_score_sent)
+            best_path.append(path_sent)
+        return best_path_scores, best_path
+            
